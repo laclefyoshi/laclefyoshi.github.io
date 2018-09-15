ef4d6342ac4112a7d0e741331c3f3fce57d102c4
SAEKI Yoshiyasu <laclef.yoshiyasu@gmail.com>
2018-09-15 23:59:45 +0900

    edit description

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Portfolio about Yoshiyasu SAEKI' }
+      { hid: 'description', name: 'description', content: 'Portfolio of Yoshiyasu SAEKI' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
